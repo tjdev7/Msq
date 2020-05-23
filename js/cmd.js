e6bf8a20e0b7c78500eaa97279a5ae574b365d6b
Toughee <MJ1menez@protonmail.com>
2020-05-23 10:44:49 -0500

    Msq/001/Initial_commit - Created the initial UI for the app - Added menu icons - Started creating code for app

---
+++ js/cmd.js
@@ -0,0 +1,52 @@
+// Pseudocode for functions
+
+
+// Logging in
+// function name1() {
+
+// }
+
+// Logging out
+// function name1() {
+
+// }
+
+// Signing up
+// function name1() {
+
+// }
+
+// Error message - signing up
+// function name1() {
+
+// }
+
+// Input form - message
+// function name1() {
+
+// }
+
+// Select a person
+// function name1() {
+
+// }
+
+// Change avatar option
+// function name1() {
+
+// }
+
+// Show message history
+// function name1() {
+
+// }
+
+//
+// function name1() {
+
+// }
+
+//
+// function name1() {
+
+// }
